058f93aec1b9a781941d7d750e2d0ce086733d82
Adrian Criollo <acriollo@namllc.net>
2023-06-16 11:20:11 -0400

    base

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-def greetings(name):
-    print(f'Hi, {name}')
+from jinja2 import Environment, FileSystemLoader
 
-greetings('John Doe')
+# Create an environment using the current directory as the template directory
+env = Environment(loader=FileSystemLoader('.'))
+
+# Define the parameters to pass to the template
+parameters = {
+    'name': 'John',
+}
+
+# Render the template with the provided parameters
+template = env.get_template('index.html')
+output = template.render(parameters)
+
+# Print or use the rendered output as needed
+print(output)
