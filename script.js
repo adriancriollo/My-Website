02499be5b674481e6b19160628f91e59a7589052
adriancriollo <45635408+adriancriollo@users.noreply.github.com>
2023-09-01 16:18:31 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const toggleButton = document.getElementById('sidebarToggle');
 const overlay = document.querySelector('.overlay');
+const closeButton = document.getElementById('closeButton');
 
 toggleButton.addEventListener('click', () => {
   overlay.classList.toggle('active'); // Toggle the 'active' class
 });
+
+closeButton.addEventListener('click', () => {
+    overlay.classList.toggle('active'); // Toggle the 'active' class
+  });
+
 window.addEventListener('resize', () => {
   if (window.innerWidth > 1000) {
     overlay.classList.remove('active');
   }
 });
+
