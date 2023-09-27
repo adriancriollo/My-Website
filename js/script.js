b7ae5d963e10b67044e60ddc52074ee5de0f75fb
adriancriollo <45635408+adriancriollo@users.noreply.github.com>
2023-09-27 17:01:03 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -150,35 +150,24 @@ adjustOpacityOnScroll(aboutmeSection);
 adjustOpacityOnScroll(projectsSection);
 adjustOpacityOnScroll(contactSection);
 
-let slideIndex = 1;
+let slideIndex = 0;
 showSlides();
 
-setInterval(() => {
-    plusSlides(1);
-}, 3500);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" activedot", "");
+    dots[i].className = dots[i].className.replace(" dotactive", "");
   }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " activedot";
+  dots[slideIndex-1].className += " dotactive";
+  setTimeout(showSlides, 10000); // Change image every 2 seconds
 }
 
 
